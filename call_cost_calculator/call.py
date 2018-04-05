d8a44fe62fda54669ae6acfa53f8b5587e42f66c
dmithamo <dennisbmithamo@gmail.com>
2018-04-05 12:45:08 +0300

    Add tests and functionality for calculation of basic cost and discount

--- call_cost_calculator/call.py
+++ call_cost_calculator/call.py
@@ -1,19 +1,27 @@
-'''This simple program models using OOP the process of calculating 
+'''This simple program models using OOP the proces of calculating 
 the cost of a telephone call. 
 '''
 
 # Needed for computation of call duration, and to determine whether call
 # is on- or off-peak.
-import datetime 
+import datetime
+
 
 class Call:
-    '''Model a call with class params as below:
+    '''Model a call with clas params as below:
 
     :param MINIMUM_COST_NEAR : The applicable minimum cost for a short distance call
     :type MINIMUM_COST_NEAR : float, constant
-    
     :param MINIMUM_COST_FAR : The applicable minimum cost for a long distance call
     :type MINIMUM_COST_FAR : float, constant
+
+    :param PER_SECOND_COST_NEAR : The applicable cost per second of a short distance call
+    :type PER-SECOND_COST_NEAR : float, constant
+
+
+    :param PER_SECOND_COST_FAR : The applicable cost per second of a long distance call
+    :type PER-SECOND_COST_FAR : float, constant
+
 
     :param OFF_PEAK_START : Time when off-peak starts
     :type OFF_PEAK_START : datetime.time object
@@ -26,48 +34,58 @@ class Call:
 
     :param VAT : The preset for boundary value for short distances
     :type VAT : float, constant
-    
+
     '''
-    MINIMUM_COST_NEAR = 0.20
-    MINIMUM_COST_FAR = 0.50
-    OFF_PEAK_START = datetime.time(19, 0, 0)
-    OFF_PEAK_END = datetime.time(6, 59, 59)
-    DISTANCE_DELIMITER = 50
-    VAT = 0.14
-    
+    # All money is in Ksh.
+    MINIMUM_COST_NEAR = 10.00
+    MINIMUM_COST_FAR = 20.00
 
-    def __init__(self, call_start_time=None, call_end_time=None, if_long_distance=None, if_share_call=None):
+    PER_SECOND_COST_NEAR = 0.50
+    PER_SECOND_COST_FAR = 0.75
+
+    OFF_PEAK_START = datetime.time(19, 0, 0)  # 19:00:00
+    OFF_PEAK_END = datetime.time(6, 59, 59)  # 06:59:59
+
+    OFF_PEAK_DISCOUNT_NEAR = 0.40
+    OFF_PEAK_DISCOUNT_FAR = 0.50
+
+    DISTANCE_DELIMITER = 50  # km
+
+    VAT = 0.14  # Charged on final cost
+
+    def __init__(self, call_start=None, call_end=None, long_distant=None, share_call=None):
         '''Return an instance of a Call object with instance variables as below
-        :param call_start_time : time when call was initiated. Format HH:MM:SS
-        :type call_start_time : datetime.time object
+        :param call_start : time when call was initiated. Format h:m:s
+        :type call_start : datetime.time object
 
-        :param call_end_time : time when call was terminated. Format HH:MM:SS
-        :type call_end_time : date.time object
+        :param call_end : time when call was terminated. Format h:m:s
+        :type call_end : date.time object
 
-        :param if_long_distance : whether call distance exceeds DISTANCE_DELIMITER
-        :type if_long_distance : boolean
+        :param long_distant : whether call distance exceeds DISTANCE_DELIMITER
+        :type long_distant : boolean
 
-        :param if_share_call : whether call was a share-call
-        :type if_share_call : boolean
+        :param share_call : whether call was a share-call
+        :type share_call : boolean
 
         '''
-        self.call_start_time = call_start_time 
-        self.call_end_time = call_end_time 
-        self.if_long_distance = if_long_distance 
-        self.if_share_call = if_share_call
+        self.call_start = call_start
+        self.call_end = call_end
+        self.long_distant = long_distant
+        self.share_call = share_call
 
     def collect_call_time(self):
-        '''Ask for user-input for call_start_time and call_end_time
+        '''Ask for user-input for call_start and call_end
         '''
-        # Determine call_start_time and call_end_time
-        call_start_time = input("\nCall started at: Use this format - HH:MM:SS\n>>")
-        call_end_time = input("\n\nCall finished at: Use this format - HH:MM:SS\n>>")
+        # Determine call_start and call_end
+        call_start = input(
+            "\nCall started at: Use this format - h:m:s\n>>")
+        call_end = input(
+            "\n\nCall finished at: Use this format - h:m:s\n>>")
 
         # Convert to objects, and save as instance variables
-        self.call_start_time = self.convert_time_to_object(call_start_time)
-        self.call_end_time = self.convert_time_to_object(call_end_time)
-        print("Started: %s\nFinished: %s" % (call_start_time, call_end_time))
-
+        self.call_start = self.convert_time_to_object(call_start)
+        self.call_end = self.convert_time_to_object(call_end)
+        return (self.call_start, self.call_end)
 
     def convert_time_to_object(self, time_input):
         '''Global method used to convert input time into a time object
@@ -75,52 +93,107 @@ class Call:
         # Time collected into a list of the hours, mins, and secs
         time_as_list = time_input.split(':')
 
-        # Extract hours (hh), minutes (mm), seconds (ss) from time_as_list and use to make time object
-        hh = int(time_as_list[0])
-        mm = int(time_as_list[1])
-        ss = int(time_as_list[2])
+        # Extract hours (h), minutes (m), seconds (s) from time_as_list and use to make time object
+        h = int(time_as_list[0])
+        m = int(time_as_list[1])
+        s = int(time_as_list[2])
 
-        time_object = datetime.time(hh, mm, ss)
-        return time_object
-    
-    def calculate_call_duration(self, call_start_time, call_end_time):
+        self.time_object = datetime.time(h, m, s)
+        return self.time_object
+
+    def calculate_call_duration(self, call_start, call_end):
         '''Calculate length of call in seconds'''
 
-        starting_time_in_secs = call_start_time.hour * 60 * 60 
-        + call_start_time.minute * 60 + call_start_time.second
+        starting_time_in_secs = call_start.hour * 3600 + \
+            call_start.minute * 60 + call_start.second
 
-        ending_time_in_secs = call_end_time.hour * 60 * 60 
-        + call_end_time.minute * 60 + call_end_time.second
+        ending_time_in_secs = call_end.hour * 3600 + \
+            call_end.minute * 60 + call_end.second
 
         self.call_duration = ending_time_in_secs - starting_time_in_secs
 
         return self.call_duration
-    
-    def determine_if_off_peak(self, call_start_time, call_end_time):
-        if call_start_time >= Call.OFF_PEAK_START and call_end_time <= Call.OFF_PEAK_END:
+
+    def determine_if_off_peak(self, call_start):
+        '''Return bool marking call as off-peak or not'''
+        if call_start >= Call.OFF_PEAK_START:
             self.is_off_peak = True
         else:
             self.is_off_peak = False
         return self.is_off_peak
-        
-    
-    def determine_if_long_distant(self):
-        call_distance = input("\nHow far off is this call made? Enter an estimate in km:\n>>")
-        if int(call_distance) >= Call.DISTANCE_DELIMITER:
-            self.if_long_distance = True
-        else:
-            self.if_long_distance = False
 
-        return self.if_long_distance
-    
+    def determine_if_long_distant(self):
+        '''Return bool indicating whether call is long distant or not'''
+        call_distance = input(
+            "\nHow far off is this call made? Enter an estimate in km:\n>>")
+
+        if int(call_distance) >= Call.DISTANCE_DELIMITER:
+            self.long_distant = True
+
+        elif int(call_distance) < Call.DISTANCE_DELIMITER:
+            self.long_distant = False
+
+        return self.long_distant
+
     def determine_if_share_call(self):
-        is_share_call = input("\nIs this a share-call? Enter 'y' for yes, 'n' for no\n>>").lower()
+        '''Return bool marking call as share_call or not'''
+        is_share_call = input(
+            "\nIs this a share-call? Enter 'y' for yes, 'n' for no\n>>").lower()
         if is_share_call == 'y':
             self.is_share_call = True
         elif is_share_call == 'n':
             self.is_share_call = False
         return self.is_share_call
+    
+    def calculate_cost(self):
+        '''Collect all params, call appropriate method for cost calculation'''
+        call_start_time, call_end_time = self.collect_call_time()
+        is_off_peak = self.determine_if_off_peak(call_start_time)
+        is_long_distant = self.determine_if_long_distant()
 
-lo_call = Call()
+        # Call appropriate cost calculation method
+        if is_long_distant:
+            call_params = self.calculate_basic_cost_long_distant(call_start_time, call_end_time, is_off_peak)
+        else:
+            call_params = self.calculate_basic_cost_short_distant(call_start_time, call_end_time, is_off_peak)
+        
+        # Display everything
+        self.presentation(call_params)
 
-print(lo_call.OFF_PEAK_END, lo_call.call_end_time, lo_call.call_start_time)
+        return call_params
+
+
+    
+    def calculate_basic_cost_long_distant(self, call_start_time, call_end_time, is_off_peak):
+        '''Calculate the basic_call_cost, without VAT or discount'''
+        call_duration = self.calculate_call_duration(call_start_time, call_end_time)
+
+        basic_cost = Call.MINIMUM_COST_FAR + (Call.PER_SECOND_COST_FAR * call_duration)
+
+        discount = 0.00 # Default value of discouunt
+        if is_off_peak:
+            discount = Call.OFF_PEAK_DISCOUNT_FAR * basic_cost
+
+        discounted_cost = basic_cost - discount
+        
+        return basic_cost, discount, discounted_cost
+    
+    def calculate_basic_cost_short_distant(self, call_start_time, call_end_time, is_off_peak):
+        '''Calculate the basic_call_cost, without VAT'''
+        call_duration = self.calculate_call_duration(call_start_time, call_end_time)
+
+        basic_cost = Call.MINIMUM_COST_NEAR + (Call.PER_SECOND_COST_NEAR * call_duration)
+
+        discount = 0.00 # Default value of discouunt
+        if is_off_peak:
+            discount = Call.OFF_PEAK_DISCOUNT_NEAR * basic_cost
+
+        discounted_cost = basic_cost - discount
+        return basic_cost, discount, discounted_cost
+    
+    def presentation(self, call_params):
+        '''Display e'erthing to user'''
+        pass
+
+# lo_call = Call()
+# lo_call.calculate_cost()
--- call_cost_calculator/tests/test_call.py
+++ call_cost_calculator/tests/test_call.py
@@ -4,7 +4,7 @@ Unittests for the Call class
 
 import unittest
 import datetime
-from call_cost_calculator.call import Call 
+from call_cost_calculator.call import Call
 
 class TestCall(unittest.TestCase):
     '''Define the parent class to contain the test cases'''
@@ -15,30 +15,44 @@ class TestCall(unittest.TestCase):
 
     def test_call_duration_calculation(self):
         '''Test the function that calculates call duration in seconds'''
-        self.assertEqual(self.my_call.calculate_call_duration(
-            datetime.time(10, 30, 30), datetime.time(12, 30, 30)), 7200)
-        
-    def test_if_time_is_of_peak(self):
-        '''Test the function that determines whether call time is off peak or not'''
-        start_time = datetime.time(19, 20, 30)
-        end_time = datetime.time(5, 20, 30)
-        self.assertEqual(self.my_call.determine_if_off_peak(start_time, end_time), True)
+        # Collect a call_start_time and call_end_time and calculate call_duration
+        call_start_time, call_end_time = self.my_call.collect_call_time()
+        call_duration = self.my_call.calculate_call_duration(
+            call_start_time, call_end_time)
+        self.assertEqual(call_duration, 7200)
 
-        start_time2 = datetime.time(11, 10, 10)
-        end_time2 = datetime.time(15, 10, 10)
-        self.assertEqual(self.my_call.determine_if_off_peak(start_time2, end_time2), False)
-        
-    def test_if_call_long_distance_calculation(self):
+    def test_off_peak_determination(self):
+        '''Test the function that determines whether call time is off peak or not'''
+        # Collect call start time and end time
+        call_start_time = self.my_call.collect_call_time()[0]
+        self.assertEqual(self.my_call.determine_if_off_peak(
+            call_start_time), True)
+
+    def test_long_distance_calculation(self):
         '''Test the function that marks a call as long distant or not'''
         self.assertEqual(self.my_call.determine_if_long_distant(), False)
         self.assertEqual(self.my_call.determine_if_long_distant(), True)
-        
-    def test_if_call_is_share_call_function(self):
+
+    def test_share_call_function(self):
         '''Test the function that marks a call as share-call or not'''
         self.assertEqual(self.my_call.determine_if_share_call(), True)
         self.assertEqual(self.my_call.determine_if_share_call(), False)
     
-        
-        
+    def test_basic_cost_calculation(self):
+        '''Test calculation of basic call cost - 5second, long distant, off-peak call'''
+        self.assertEqual(self.my_call.calculate_cost(), (23.75, 11.875, 11.875))
+
+    def test_basic_cost_calculation_2(self):
+        '''Test calculation of basic call cost - 5second, long distant, not off-peak call'''
+        self.assertEqual(self.my_call.calculate_cost(), (23.75, 0.00, 23.75))
+    
+    def test_basic_cost_calculation_3(self):
+        '''Test calculation of basic call cost - 5second, short distant, off-peak call'''
+        self.assertEqual(self.my_call.calculate_cost(), (12.50, 5.0, 7.50))
+
+    def test_basic_cost_calculation_4(self):
+        '''Test calculation of basic call cost - 5second, short distant, not off-peak call'''
+        self.assertEqual(self.my_call.calculate_cost(), (12.50, 0.00, 12.50))
+
 if __name__ == '__main__':
     unittest.main()
